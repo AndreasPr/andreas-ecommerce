65bb626e473d8b960db65fd47c4d180a02f18c32
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2020-05-01 14:50:16 -0400

    Stripe Integration

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import {Route} from 'react-router-dom';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
 const ShopPage = ({match}) => (
     <div className='shop-page'>
+        {console.log(match)}
         <Route exact path={`${match.path}`} component={CollectionsOverview} />
         <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
