55536a5dbed5523b03ce8b42136ec389ec0b033d
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2020-06-26 07:24:20 -0400

    Convertion of the app to PWA, adding serviceWorker, HTTPS and manifest

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
+import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import App from './App';
@@ -19,3 +20,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+serviceWorker.register();
