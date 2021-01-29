48cc0adaff9667564a853c655335a834cd4f8ad3
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2021-01-29 15:36:09 -0500

    remove unused css

--- client/src/index.js
+++ client/src/index.js
@@ -8,8 +8,6 @@ import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
-
-import './index.css';
 import App from './App';
 import {PersistGate} from 'redux-persist/integration/react';
 import {store, persistor} from './redux/store';
