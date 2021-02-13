9c5702551981dd179442e28406e9cf8c852a8698
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2021-02-12 22:39:51 -0500

    changes to service worker to server and add service worker to public folder

--- server.js
+++ server.js
@@ -52,9 +52,9 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, 'client/build')));
 
 
-    app.get("/service-worker.js", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "public", "service-worker.js"));
-      });
+    // app.get("/service-worker.js", (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, "public", "service-worker.js"));
+    //   });
 
     app.get('*', function(request, response){
         response.sendFile(path.join(__dirname, 'client/build', 'index.html'))
@@ -68,9 +68,9 @@ app.listen(port, error => {
     console.log('Server is running on the port ' + port);
 });
 
-app.get('/service-worker.js', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
-});
+// app.get('./service-worker.js', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+// });
 
 app.post('/payment', (request, response) => {
     const body = {
