37f11753f51ab557d260435cd5b5d61c42dffb1a
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2021-02-12 22:51:28 -0500

    changes to service worker to server

--- server.js
+++ server.js
@@ -68,9 +68,9 @@ app.listen(port, error => {
     console.log('Server is running on the port ' + port);
 });
 
-// app.get('./service-worker.js', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
-// });
+app.get('./service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
 
 app.post('/payment', (request, response) => {
     const body = {
