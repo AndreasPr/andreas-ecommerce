265eb1dca73e7e45e813e96bb25bc2ca67326a33
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2021-02-13 11:13:02 -0500

    changes for service worker in server

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get("/service-worker.js", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client/public", "service-worker.js"));
+        res.sendFile(path.resolve(__dirname, "public", "service-worker.js"));
       });
 
     app.get('*', function(request, response){
