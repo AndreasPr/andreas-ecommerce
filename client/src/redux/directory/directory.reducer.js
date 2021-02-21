3fcb964a7765da0deb517287712f6082faec0a14
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2021-02-21 08:13:40 -0500

    changes to images from webp to jpg

--- client/src/redux/directory/directory.reducer.js
+++ client/src/redux/directory/directory.reducer.js
@@ -2,32 +2,32 @@ const INITIAL_STATE = {
     sections: [
         {
           title: 'hats',
-          imageUrl: 'https://i.ibb.co/85vjXNn/main-hats.webp',
+          imageUrl: 'https://i.ibb.co/VMLkWZZ/main-hats.jpg',
           id: 1,
           linkUrl: 'shop/hats'
         },
         {
           title: 'jackets',
-          imageUrl: 'https://i.ibb.co/Srsxpj5/main-jackets.webp',
+          imageUrl: 'https://i.ibb.co/9snvvqj/main-jackets.jpg',
           id: 2,
           linkUrl: 'shop/jackets'
         },
         {
           title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/n8rdS33/main-sneakers.webp',
+          imageUrl: 'https://i.ibb.co/HHQBbDJ/main-sneakers.jpg',
           id: 3,
           linkUrl: 'shop/sneakers'
         },
         {
           title: 'womens',
-          imageUrl: 'https://i.ibb.co/GcfknGR/main-women.webp',
+          imageUrl: 'https://i.ibb.co/khHbHzT/main-women.jpg',
           size: 'large',
           id: 4,
           linkUrl: 'shop/womens'
         },
         {
           title: 'mens',
-          imageUrl: 'https://i.ibb.co/7KvktsG/main-men.webp',
+          imageUrl: 'https://i.ibb.co/QFY2hLn/main-men.jpg',
           size: 'large',
           id: 5,
           linkUrl: 'shop/mens'
