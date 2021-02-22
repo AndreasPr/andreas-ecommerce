4b3364a560598e6a30232df9727e5a14a45a9b99
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2021-02-21 20:28:57 -0500

    addition of search bar in collection page

--- client/src/pages/collection/collection.component.jsx
+++ client/src/pages/collection/collection.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './collection.styles.css';
 //import { useLocation } from 'react-router-dom'
 import CollectionItem from '../../components/collection-item/collection-item.component';
@@ -9,8 +9,28 @@ const CollectionPage = ({match, collection}) => {
     // Destructuring Assignment, which on the left-hand side of the assignment we define what 
     // values to unpack from the sourced object - collection 
     const {title, items, itemNameRoute} = collection;
+
+    const [isDisplayedProducts, setIsDisplayedProducts] = useState(items);
+
+    const handleChange = (event) => {
+        let searchQuery = event.target.value.toLowerCase();
+        let displayedProducts = items.filter(function(el){
+            let searchValue = el.name.toLowerCase();
+            
+            return searchValue.indexOf(searchQuery) !== -1;
+        });
+        setIsDisplayedProducts(displayedProducts);
+    };
+
     return(    
         <div className="container-fluid collectionPageContainer">
+            
+            <div className="row">
+                <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 inputSearchContainer">
+                    <input name="inputSearch" className="form-control inputSearch" onChange={handleChange} placeholder="Search your product..."/>
+                </div>
+            </div>            
+
             <div className="row">
                 <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center collectionPageTitle">
                     <h3 className="heading-collectionPageTitle">{title.toUpperCase()}</h3>
@@ -19,6 +39,17 @@ const CollectionPage = ({match, collection}) => {
             
             <div className="row">
                 {
+                    isDisplayedProducts 
+                    ? 
+                    isDisplayedProducts
+                    .map((displayProduct) => 
+                        (
+                            <div key={displayProduct.id} className="col-12 d-flex justify-content-center col-sm-6 col-md-6 col-lg-4 col-xl-3 collectionPageProducts">
+                                <CollectionItem key={displayProduct.id} item={displayProduct} title={title} itemNameRoute={itemNameRoute}/>
+                            </div>
+                        )
+                    )
+                    :
                     items
                     .map((item) => 
                         (
